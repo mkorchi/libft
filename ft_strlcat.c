36b4d9ed2513f21de4ae2573949afc7442435312
Mohamed Korchi <mkorchi@e1r1p7.1337.ma>
2021-11-07 20:19:49 +0100

    fixed strlcat and strlcpy

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,23 +6,11 @@
 /*   By: mkorchi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 14:08:02 by mkorchi           #+#    #+#             */
-/*   Updated: 2021/11/06 14:22:36 by mkorchi          ###   ########.fr       */
+/*   Updated: 2021/11/07 20:18:55 by mkorchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static size_t	ft_nstrlen(const char	*src)
-{
-	size_t	size;
-
-	if (src == NULL)
-		return (0);
-	size = 0;
-	while (src[size])
-		size++;
-	return (size);
-}
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -30,16 +18,19 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	j;
 	int		n;
 
-	dstlen = ft_nstrlen(dst);
+	dstlen = ft_strlen(dst);
 	n = dstsize - dstlen - 1;
 	if (n < 0)
 		return (dstsize + ft_strlen(src));
-	j = 0;
-	while ((int) j < n && src[j])
+	if (n > 0)
 	{
-		dst[dstlen + j] = src[j];
-		j++;
+		j = 0;
+		while ((int) j < n && src[j])
+		{
+			dst[dstlen + j] = src[j];
+			j++;
+		}
+		dst[dstlen + j] = '\0';
 	}
-	dst[dstlen + j] = '\0';
 	return (dstlen + ft_strlen(src));
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mkorchi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 13:21:14 by mkorchi           #+#    #+#             */
-/*   Updated: 2021/11/03 11:28:44 by mkorchi          ###   ########.fr       */
+/*   Updated: 2021/11/07 19:20:29 by mkorchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	n;
 
 	i = 0;
-	n = 0;
-	while (src[n])
-		n++;
+	n = ft_strlen(src);
+	if (!*src && !*dst)
+		return (0);
 	if (dstsize == 0)
 		return (n);
 	while (i < dstsize - 1 && src[i])
