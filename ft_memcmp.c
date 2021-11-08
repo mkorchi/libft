f06b7f1485b2ff9bff7d1bc22598013d0666badd
Mohamed Korchi <mkorchi@e2r5p3.1337.ma>
2021-11-08 10:03:14 +0100

    casting to unsigned char the comp in cmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mkorchi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 12:01:46 by mkorchi           #+#    #+#             */
-/*   Updated: 2021/11/04 14:43:56 by mkorchi          ###   ########.fr       */
+/*   Updated: 2021/11/08 09:48:36 by mkorchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	str2 = (unsigned char *) s2;
 	while (i < n)
 	{
-		if (str1[i] != str2[i])
+		if ((unsigned char) str1[i] != (unsigned char) str2[i])
 			return ((unsigned char) str1[i] - (unsigned char) str2[i]);
 		i++;
 	}
